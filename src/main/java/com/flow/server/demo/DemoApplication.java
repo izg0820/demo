0ad41305c458f6ad5b95dd00b836af92d98f9c19
izg0820 <izg0820@naver.com>
2022-06-18 01:15:57 +0900

    feature: 서버 시작시 고정확장자 추가되는 서비스 생성

--- src/main/java/com/flow/server/demo/DemoApplication.java
+++ src/main/java/com/flow/server/demo/DemoApplication.java
@@ -1,15 +1,24 @@
 package com.flow.server.demo;
 
 import com.flow.server.demo.utils.InitService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+@AllArgsConstructor
+public class DemoApplication implements CommandLineRunner {
 
+	private final InitService initService;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		InitService initService = new InitService();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		initService.init();
 	}
 
--- src/main/java/com/flow/server/demo/utils/InitService.java
+++ src/main/java/com/flow/server/demo/utils/InitService.java
@@ -1,20 +1,28 @@
 package com.flow.server.demo.utils;
 
 import com.flow.server.demo.controller.FileExtensionController;
+import com.flow.server.demo.domain.FileExtension;
+import com.flow.server.demo.domain.repository.FileExtensionRepository;
 import com.flow.server.demo.dto.FileExtensionRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+@Component
+@AllArgsConstructor
 public class InitService {
 
-    @Autowired
-    private FileExtensionController fileExtensionController;
+    private final FileExtensionRepository fileExtensionRepository;
 
     public void init() {
         String[] fixedExtensions = Constant.FixedExtenstion;
         for (String fixedExtension : fixedExtensions) {
-            fileExtensionController.save(new FileExtensionRequestDto(fixedExtension, true));
+            fileExtensionRepository.save(new FileExtension(fixedExtension, true));
         }
     }
 }
